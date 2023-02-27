81a32b51086301662218a8682f1ae1eec7bb97be
Clyde Choi <clydechoi@gmail.com>
2023-02-27 17:28:21 +0100

    add bookings model without dinosaur reference

---
+++ db/migrate/20230227162740_create_bookings.rb
@@ -0,0 +1,10 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.boolean :confirmation_status
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
