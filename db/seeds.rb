cd817e6e3a0a652fda4f3fad9198db083ebed2c9
Manuela Baecker <manu.baecker@t-online.de>
2023-02-28 12:16:03 +0100

    dino seeds file, index.html.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Dinosaur.create(name: "rexi", description: "very good dino", age: "579275937 years")
-Dinosaur.create(name: "dinoli", description: "very bad dino", age: "83759275932875987 years")
+puts "cleaning DB"
+Dinosaur.destroy_all
+puts "creating new dinos"
+Dinosaur.create(name: "Nori", description: "Nori is from the US and loves hot climate. Good if you have some trees around for his vegetarian diet", age: "89 million years", user: User.last)
+Dinosaur.create(name: "T-Rexo", description: "Rexo is a bit aggressive, be careful if you have dogs.", age: "66 million years", user: User.first)
+Dinosaur.create(name: "Gwen", description: "Flying Reptile, he loves his freedom and likes flying at the beach", age: "38 million years", user: User.first)
+Dinosaur.create(name: "Helmut", description: "Helmut likes long walks in the forest", age: "155 million years", user: User.last)
+
+
+puts "done"
