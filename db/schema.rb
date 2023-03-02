5f9a788af77e01026c53b14df0a76f852a83727b
Clyde Choi <clydechoi@gmail.com>
2023-03-02 11:10:11 +0100

    removed default, added validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_094229) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_100814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_094229) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.boolean "confirmation_status", default: false
+    t.boolean "confirmation_status"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
